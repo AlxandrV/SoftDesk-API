5fd914c87404640906e9dadf56f1706e7b701ee9
AlxandrV <a.vigneaud@gmail.com>
2022-06-20 17:56:48 +0200

    Add - update destroy method, verify is user at the author of project

--- SoftDesk/support/views.py
+++ SoftDesk/support/views.py
@@ -1,12 +1,14 @@
 from pydoc import importfile
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db.models import Q
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import RegisterSerializer, ProjectListSerializer
-from .models import Projects, Issues, Comments
+from .models import Projects, Issues, Comments, Contributors
 
 class RegisterView(generics.CreateAPIView):
 
@@ -18,3 +20,10 @@ class ProjectViewset(ModelViewSet):
     serializer_class = ProjectListSerializer
     queryset = Projects.objects.all()
     permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, pk=None):
+        project = Projects.objects.get(id=pk)
+        author = Contributors.objects.filter(Q(project=project) & Q(role='AUTH'))[0]
+        if author.user != request.user:
+            return JsonResponse({"Permission": "You're not allowed to delete this project."})
+        return super().destroy(request, pk)
